c12736f1bf812b0e865003ab73f85c2f176a94c4
Will Burnette <will@hsdesignhouse.com>
2015-06-14 14:40:12 -0500

    Implemented product list page

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,13 @@ class Product < ActiveRecord::Base
 	def full_description
 		"#{self.title} #{self.subtitle}"
 	end
+	
 	def price_in_cents
 (self.price * 100).to_i
   end
+
+  def thumbnail_image_name
+  	"product-#{title.split.slice(0..-2).join(" ").gsub(" ", "-").downcase}.png"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,12 @@ root 'pages#home'
   # Support stripe payments through charges
   resources :charges
 
+  # Route for showing products
+  resources :products, only: [:index]
+
   # Route for showing purchases
   resources :purchases, only: [:show]
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
